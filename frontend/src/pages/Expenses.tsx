90c7c5ab84cc36c9eacc83d38b343a55e143e2cf
sean <custodiocsean@gmail.com>
2026-02-03 12:56:14 +0800

    fix build errors

--- frontend/src/pages/Expenses.tsx
+++ frontend/src/pages/Expenses.tsx
@@ -1,8 +1,4 @@
-import React from "react";
 import { ExpenseTable } from "../components/Expenses";
-import expense from "../services/api/expenseQuery";
-import { useQueryClient } from "@tanstack/react-query";
-import { iUser } from "../types/user";
 const Expenses = () => {
   return (
     <div className="p-4 sm:ml-64">
